b05ceab06e47bf5f2f485151e8119d79962e1f72
Md Shahriar Hossain Rhythm <rhythmshahriar0@gmail.com>
2021-03-12 19:29:13 +0600

    last commit

--- src/components/TeamInfo/TeamInfo.js
+++ src/components/TeamInfo/TeamInfo.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCertificate, faFutbol, faMars, faFlag } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import './TeamInfo.css';
 
 const TeamInfo = () => {
@@ -14,7 +15,7 @@ const TeamInfo = () => {
             .then(data => setTeam(data.teams[0]))
     }, [])
 
-    const { strTeam, intFormedYear, strCountry, strSport, strGender, strTeamBadge } = team;
+    const { strTeam, intFormedYear, strCountry, strSport, strGender, strTeamBadge, strFacebook, strTwitter, strYoutube } = team;
 
     // conditional rendering
 
@@ -32,17 +33,16 @@ const TeamInfo = () => {
                     <div className="row ">
                         <div className="Info-div col-md-6">
                             <h1>Team name: {strTeam}</h1>
-                            <h5>Founded: {intFormedYear} </h5>
-                            <h5>Country: {strCountry}</h5>
-                            <h5>Sports Type: {strSport}</h5>
-                            <h5>Gender: {strGender}</h5>
+                            <h5><FontAwesomeIcon icon={faCertificate} />Founded: {intFormedYear} </h5>
+                            <h5><FontAwesomeIcon icon={faFlag} />Country: {strCountry}</h5>
+                            <h5><FontAwesomeIcon icon={faFutbol} />Sports Type: {strSport}</h5>
+                            <h5><FontAwesomeIcon icon={faMars} />Gender: {strGender}</h5>
                         </div>
 
-                        <div className="col-md-6 gender-div">
+                        <div className="col-md-6 gender-div d-flex justify-content-center">
                             {playersImage}
                         </div>
                     </div>
-
                 </div>
 
                 <div>
@@ -51,10 +51,11 @@ const TeamInfo = () => {
                     <p className="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero harum doloribus a veritatis eum fugit. Repudiandae ullam nobis vero accusantium excepturi atque porro error minima sunt sint, est tenetur maiores perferendis repellat beatae consectetur veniam, maxime culpa quia reprehenderit? Cumque reprehenderit assumenda distinctio blanditiis quia earum eveniet libero inventore optio.</p>
                 </div>
 
-                <div className="icons">
-                    <h2><FontAwesomeIcon icon={faCoffee} /></h2>
+                <div className="icons d-flex justify-content-center">
+                    <h2><a href="https://web.programming-hero.com/dashboard"><FontAwesomeIcon icon={faFacebookF} /></a></h2>
+                    <h2><a href="https://web.programming-hero.com/dashboard"><FontAwesomeIcon icon={faTwitter} /></a></h2>
+                    <h2><a href="https://web.programming-hero.com/dashboard"><FontAwesomeIcon icon={faYoutube} /></a></h2>
                 </div>
-
             </div>
         </section>
     );
